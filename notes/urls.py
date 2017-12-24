3b37ce11300d3cfac42398ddea3b73ffb2d275e5
aQwus <aqwusjargon@gmail.com>
2017-12-25 02:01:42 +0530

    addnotes

--- notes/urls.py
+++ notes/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from notesite.views import loggedin,auth_view,register
-from .views import index_view
+from .views import index_view,add_note
 
 app_name='notes'
 
 urlpatterns = [
     url(r'^loggedin/$',loggedin,name='loggedin'),
     url(r'^auth/$',auth_view,name='authenticate'),
+    url(r'^addnote/', add_note, name='addnote'),
     url(r'^$', index_view, name='index'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from notes.models import Note
+from notes.forms import NoteForm
 
 
 def index_view(request):
 	notes = Note.objects.all()
 	return render(request, 'notes/index.html', {'notes' : notes})
+
+
+def add_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('notes:index'))
+
+    else:
+        form = NoteForm()
+
+    return render(request, 'notes/addnote.html', {'form': form})
